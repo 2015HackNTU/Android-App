3026a00e481da4bb577e6daa891e9e97495a8f55
EddieW <eddiew@stanford.edu>
2015-01-26 16:35:31 -0800

    notification history

---
+++ app/src/main/java/com/treehacks/treehacks/AnnounceAdapter.java
@@ -0,0 +1,57 @@
+package com.treehacks.treehacks;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.parse.ParseObject;
+
+import java.util.List;
+
+/**
+ * Created by eddie_000 on 1/25/2015.
+ */
+public class AnnounceAdapter extends RecyclerView.Adapter<AnnounceAdapter.ViewHolder> {
+    public List<ParseObject> announcements;
+
+    public AnnounceAdapter(List<ParseObject> announcements) {
+        this.announcements = announcements;
+    }
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView title;
+        public TextView description;
+
+        public ViewHolder (CardView cardView) {
+            super(cardView);
+            title = (TextView) cardView.findViewById(R.id.card_title);
+            description = (TextView) cardView.findViewById(R.id.card_description);
+        }
+    }
+
+    @Override
+    public AnnounceAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        CardView v = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.view_announce, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(AnnounceAdapter.ViewHolder viewHolder, int i) {
+        ParseObject announcement = announcements.get(i);
+        viewHolder.title.setText(announcement.getString("title"));
+        viewHolder.description.setText(announcement.getString("description"));
+    }
+
+    @Override
+    public int getItemCount() {
+        return announcements.size();
+    }
+}
---
+++ app/src/main/java/com/treehacks/treehacks/AnnounceFragment.java
@@ -0,0 +1,97 @@
+package com.treehacks.treehacks;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.parse.DeleteCallback;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.SaveCallback;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Eddie on 1/20/2015.
+ */
+public class AnnounceFragment extends Fragment {
+    RecyclerView announceView;
+    AnnounceAdapter announceAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+
+        // Sync announcements from cloud
+        ParseQuery<ParseObject> cloudQuery = ParseQuery.getQuery("Push");
+        cloudQuery.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(final List<ParseObject> parseCloudAnnouncements, ParseException e) {
+                if (e == null) {
+                    // Update local datastore
+                    ParseObject.unpinAllInBackground("announcements", new DeleteCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                ParseObject.pinAllInBackground("announcements", parseCloudAnnouncements, new SaveCallback() {
+                                    @Override
+                                    public void done(ParseException e) {
+                                        if (e == null)
+                                            Log.d("Parse", "cache announcements from cloud SUCCESS");
+                                        else
+                                            e.printStackTrace();
+                                    }
+                                });
+                            }
+                            else {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                    if (announceAdapter != null) {
+                        announceAdapter.announcements = parseCloudAnnouncements;
+                        announceAdapter.notifyDataSetChanged();
+                    }
+                } else {
+                    e.printStackTrace();
+                    // We done fucked up
+                }
+            }
+        });
+    }
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_announce, container, false);
+		announceView = (RecyclerView) rootView.findViewById(R.id.announce_view);
+        announceView.setHasFixedSize(true);
+        announceView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        // Get scheduled events from cache
+        ParseQuery<ParseObject> localQuery = ParseQuery.getQuery("Push");
+        localQuery.fromLocalDatastore();
+        List<ParseObject> pushes;
+        try {
+            pushes = localQuery.find();
+            Log.d("Parse", "read from local db SUCCESS");
+        } catch (ParseException e) {
+            e.printStackTrace();
+            pushes = new ArrayList<>();
+        }
+
+        announceAdapter = new AnnounceAdapter(pushes);
+        announceView.setAdapter(announceAdapter);
+        return rootView;
+	}
+
+
+}
