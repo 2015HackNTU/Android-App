99bc40e4e0b6c3426f7db3f854022fc516d1c8ce
EddieW <eddiew@stanford.edu>
2015-02-01 13:04:29 -0800

    add faq fragment

---
+++ app/src/main/java/com/treehacks/treehacks/FaqFragment.java
@@ -0,0 +1,11 @@
+package com.treehacks.treehacks;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by eddie_000 on 1/30/2015.
+ */
+public class FaqFragment extends Fragment {
+
+    @Override
+}
