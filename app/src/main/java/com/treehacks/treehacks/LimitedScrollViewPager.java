99dc37d5dbd65c7fa9639acc80993b49f5cb9711
eddiew <eddiew@stanford.edu>
2015-02-04 14:13:27 -0800

    allow scrolling on pager title strip

--- app/src/main/java/com/treehacks/treehacks/LimitedScrollViewPager.java
+++ app/src/main/java/com/treehacks/treehacks/LimitedScrollViewPager.java
@@ -3,8 +3,11 @@ package com.treehacks.treehacks;
 import android.content.Context;
 import android.graphics.Point;
 import android.os.Build;
+import android.support.v4.view.PagerTabStrip;
+import android.support.v4.view.PagerTitleStrip;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.WindowManager;
 
@@ -18,6 +21,8 @@ import java.util.HashSet;
 public class LimitedScrollViewPager extends ViewPager {
 	Context context;
 	HashSet<Integer> ignoredPointers;
+	PagerTitleStrip pagerTitleStrip;
+	int pagerTitleStripBottom;
 
 	private void init (Context context) {
 		this.context = context;
@@ -36,21 +41,33 @@ public class LimitedScrollViewPager extends ViewPager {
 
 	@Override
 	public boolean onTouchEvent(MotionEvent ev) {
-		if (ignoredPointers.contains(ev.getPointerId(0))) {
-			return false;
-		}
-		return super.onTouchEvent(ev);
+		return !ignoredPointers.contains(ev.getPointerId(0)) && super.onTouchEvent(ev);
+	}
+
+	// Get position of bottom of pagerTitleStrip so scrolling is not disabled above it
+	@Override
+	public void onLayout(boolean changed, int left, int top, int right, int bottom) {
+		super.onLayout(changed, left, top, right, bottom);
+		pagerTitleStrip = (PagerTitleStrip) findViewById(R.id.pager_title_strip);
+		int[] location = new int[2];
+		pagerTitleStrip.getLocationOnScreen(location);
+		pagerTitleStripBottom = location[1] + pagerTitleStrip.getBottom();
 	}
 
 	// Limits scrolling when displaying schedule (index 0)
 	// Remembers pointer IDs of down events in the center of the screen
 	// and ignores them in onTouchEvent
 	// Allows scrolling if the user touched the side of the device
-	// TODO: allow view scrolling on the pager title strip
 	@Override
 	public boolean onInterceptTouchEvent(MotionEvent ev) {
+
+		// Only limit scrolling for schedule view
 		if (getCurrentItem() == 0) {
+
+			// Capture down events to ignore pointer
 			if (ev.getActionMasked() == MotionEvent.ACTION_DOWN) {
+
+				// Get window width
 				WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
 				int width;
 				if (Build.VERSION.SDK_INT > Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
@@ -61,7 +78,9 @@ public class LimitedScrollViewPager extends ViewPager {
 				else {
 					width = wm.getDefaultDisplay().getWidth();
 				}
-				if (ev.getRawX() < width / 8 || width * 7 / 8 < ev.getRawX()) {
+
+				// Allow scrolling if started in edge 1/8 of screen, or above schedule view
+				if (ev.getRawX() < width / 8 || width * 7 / 8 < ev.getRawX() || ev.getRawY() < pagerTitleStripBottom) {
 					return true;
 				}
 				else {
@@ -69,10 +88,9 @@ public class LimitedScrollViewPager extends ViewPager {
 					return false;
 				}
 			}
+			// un-ignore pointer on up events
 			else if (ev.getActionMasked() == MotionEvent.ACTION_UP) {
-				int pointerId = ev.getPointerId(0);
-				if (ignoredPointers.contains(pointerId))
-					ignoredPointers.remove(pointerId);
+				ignoredPointers.remove(ev.getPointerId(0));
 			}
 		}
 		return super.onInterceptTouchEvent(ev);
